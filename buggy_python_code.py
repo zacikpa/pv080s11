eff57a56e33bbdc3240b849bfc719bb3ef0569ca
Pavol Žáčik <xzacik@fi.muni.cz>
2022-04-24 17:05:21 +0200

    Fix some issues

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,9 +1,8 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
+import pickle
 import subprocess
 import base64
-import subprocess
 import flask
 
 # Input injection
@@ -17,9 +16,8 @@ def foo(request, user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
-
 # Pickles
-class RunBinSh(object):
+class RunBinSh():
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
